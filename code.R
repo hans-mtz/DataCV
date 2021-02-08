c204a19073ebad8305ed562fc10246be126722c1
hans-mtz <hans.yoel.martinez@gmail.com>
2021-02-08 13:05:29 -0500

    updating CV links
    Updating links

--- code.R
+++ code.R
@@ -1,3 +1,11 @@
 # install.packages("devtools")
 devtools::install_github("nstrayer/datadrivencv")
 
+# run ?datadrivencv::use_datadriven_cv to see more details
+datadrivencv::use_datadriven_cv(
+  full_name = "Hans Martinez",
+  data_location = "https://docs.google.com/spreadsheets/d/1JoK4tb-81ruPrv553DzY-JSIKSaCR9TW80i8--qhuqE/edit?usp=sharing",
+  pdf_location = "https://github.com/hans-mtz/DataCV/raw/master/hans_cv.pdf",
+  html_location = "https://github.com/hans-mtz/DataCV",
+  source_location = "https://github.com/hans-mtz/DataCV"
+)
--- render_cv.r
+++ render_cv.r
@@ -7,15 +7,15 @@
 
 # Knit the HTML version
 rmarkdown::render("cv.rmd",
-                  params = list(pdf_mode = FALSE),
+                  params = list(pdf_mode = TRUE),
                   output_file = "cv.html")
 
 # Knit the PDF version to temporary html location
-tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+# tmp_html_cv_loc <- fs::file_temp(ext = ".html")
 rmarkdown::render("cv.rmd",
-                  params = list(pdf_mode = TRUE),
-                  output_file = tmp_html_cv_loc)
+                  params = list(pdf_mode = FALSE),
+                  output_file = "cv_pdf.html")
 
 # Convert to PDF using Pagedown
-pagedown::chrome_print(input = tmp_html_cv_loc,
-                       output = "cv.pdf")
+# pagedown::chrome_print(input = tmp_html_cv_loc,
+#                        output = "cv.pdf")
